3b53a4ede5fcea923efa7b4aed7a43a79381b159
Lucas Wiens <lucas.wiens@desy.de>
2021-05-05 15:31:34 +0200

    Adjust a few lines for convenience in the findMissingBins.sh

--- TTHAnalysis/python/plotter/susy-1lep/RcsDevel/findMissingBins.sh
+++ TTHAnalysis/python/plotter/susy-1lep/RcsDevel/findMissingBins.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-# Get a list of all missing bins in target output directory
-ls ${1}/grid/ | grep ".root" | sed "s@.yields.root@@g" |sort |uniq > producedBins_grid.md
-ls ${1}/scan/ | grep ".root" | sed "s@.yields.root@@g" |sort |uniq > producedBins_scan.md
-ls ${1}/grid-dilep/ | grep ".root" | sed "s@.yields.root@@g" |sort |uniq > producedBins_grid-dilep.md
-
+input=$1
+year=$2
 # List all current search bins
 python searchBins_0b_with_NW.py | sort | uniq > searchBins.md
-
-# Produce a file with only the missing bins which can be read in by searchBins.py as a missingBins list which delets all bins from the cutDict to only produce the missing bins
-grep -v -x -f producedBins_grid.md searchBins.md | sed "s@_SR@@g" | sed "s@_CR@@g" | sort | uniq > missingBins_grid.md
-grep -v -x -f producedBins_scan.md searchBins.md | sed "s@_SR@@g" | sed "s@_CR@@g" | sort | uniq > missingBins_scan.md
-grep -v -x -f producedBins_grid-dilep.md searchBins.md | sed "s@_SR@@g" | sed "s@_CR@@g" | sort | uniq > missingBins_grid-dilep.md
+# Get a list of all missing bins in target output directory
+for dir in grid scan grid-dilep;
+do
+	ls ${input}/${dir} | grep ".root" | sed "s@.yields.root@@g" |sort |uniq > producedBins_${dir}_${year}.md
+	grep -v -x -f producedBins_${dir}_${year}.md searchBins.md | sed "s@_SR@@g" | sed "s@_CR@@g" | sort | uniq > missingBins_${dir}_${year}.md
+done
